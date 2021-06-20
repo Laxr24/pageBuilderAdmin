300e056a39bb38c604db58f4d242576d28e7f82b
K. M. Shaan <shaan@Ks-MacBook-Pro.local>
2021-06-20 11:18:43 +0600

    Branded

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WTS
                 </div>
 
                 <div class="links">
